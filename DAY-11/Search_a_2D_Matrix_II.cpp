dd447f998d76720da3a607abcada04f9e71446f4
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-04-17 23:31:16 +0530

    Day 11

---
+++ DAY-11/Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+        
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        int r = 0;
+        int c = cols - 1;
+        
+        while (r < rows && c >= 0) {
+            if (matrix[r][c] == target) {
+                return true;
+            } else if (matrix[r][c] > target) {
+                c--;
+            } else {
+                r++;
+            }
+        }
+        
+        return false;
+    }
+};

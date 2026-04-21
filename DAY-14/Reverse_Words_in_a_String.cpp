d6281ac3e6d36b3d991759d5fd7345ad94fd121b
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-04-21 20:33:50 +0530

    Day 14

---
+++ DAY-14/Reverse_Words_in_a_String.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res = "";
+        int n = s.length();
+        int i = 0;
+
+        while (i < n) {
+            while (i < n && s[i] == ' ') i++;
+            if (i >= n) break;
+
+            int j = i;
+            while (j < n && s[j] != ' ') j++;
+
+            string word = s.substr(i, j - i);
+
+            if (res.length() == 0) res = word;
+            else res = word + " " + res;
+
+            i = j;
+        }
+        return res;
+    }
+};

9a3ce8660b0f14a6ff080cf1de0eb8022ad63fca
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-04-28 20:54:45 +0530

    Day 18

---
+++ DAY-19/Palindrome_Partitioning.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(const string& s, int start, int end) {
+        while (start < end) {
+            if (s[start++] != s[end--]) return false;
+        }
+        return true;
+    }
+
+    void backtrack(int index, string& s, vector<string>& path, vector<vector<string>>& res) {
+        if (index == s.length()) {
+            res.push_back(path);
+            return;
+        }
+
+        for (int i = index; i < s.length(); ++i) {
+            if (isPalindrome(s, index, i)) {
+                path.push_back(s.substr(index, i - index + 1));
+                backtrack(i + 1, s, path, res);
+                path.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> path;
+        backtrack(0, s, path, res);
+        return res;
+    }
+};

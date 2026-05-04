964dc173d6005b3a90497cf01db43623cac215c2
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-05-04 18:54:55 +0530

    Day 25

---
+++ DAY-25/Copy_List_with_Random_Pointer.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        Node* curr = head;
+        while (curr) {
+            Node* copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+            curr = copy->next;
+        }
+
+        curr = head;
+        while (curr) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+
+        Node* newHead = head->next;
+        Node* copyCurr = newHead;
+        curr = head;
+
+        while (curr) {
+            curr->next = curr->next->next;
+            if (copyCurr->next) {
+                copyCurr->next = copyCurr->next->next;
+            }
+            curr = curr->next;
+            copyCurr = copyCurr->next;
+        }
+
+        return newHead;
+    }
+};

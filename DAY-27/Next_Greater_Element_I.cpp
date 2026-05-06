92910e8c4f8307101e51ce614808e11ab390bed9
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-05-06 21:07:03 +0530

    Day 27

---
+++ DAY-27/Next_Greater_Element_I.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> next_greater;
+        stack<int> s;
+
+        for (int num : nums2) {
+            while (!s.empty() && num > s.top()) {
+                next_greater[s.top()] = num;
+                s.pop();
+            }
+            s.push(num);
+        }
+
+        vector<int> ans;
+        for (int num : nums1) {
+            if (next_greater.count(num)) {
+                ans.push_back(next_greater[num]);
+            } else {
+                ans.push_back(-1); 
+            }
+        }
+
+        return ans;
+    }
+};

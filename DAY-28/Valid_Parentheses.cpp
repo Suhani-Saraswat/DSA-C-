1ab0c32ff6bd74f02d52068fd7926caca0f3e698
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-05-08 23:48:11 +0530

    Day 28

---
+++ DAY-28/Valid_Parentheses.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+                if (st.empty()) return false;
+                if (c == ')' && st.top() != '(') return false;
+                if (c == '}' && st.top() != '{') return false;
+                if (c == ']' && st.top() != '[') return false;
+                
+                st.pop();
+            }
+        }
+        
+        return st.empty();
+    }
+};

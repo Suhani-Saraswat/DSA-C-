6cf88da467186be9910823d196a9a91d36b1e98b
Suhani Saraswat <suhanisaraswat1795@gmail.com>
2026-05-08 23:49:19 +0530

    Day 28

---
+++ DAY-28/First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[26] = {0};
+        
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
